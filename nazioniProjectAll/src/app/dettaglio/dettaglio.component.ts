6adbda847b00dc90336c2fc82ada4e0a3dcc6ebd
francescopaolo97 <154432200+francescopaolo97@users.noreply.github.com>
2024-03-22 10:25:53 +0100

    Add files via upload

--- nazioniProjectAll/src/app/dettaglio/dettaglio.component.ts
+++ nazioniProjectAll/src/app/dettaglio/dettaglio.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { NazioniService } from '../services/nazioni.service';
+import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { NazioniService } from "../services/nazioni.service";
+import * as L from "leaflet";
 
 @Component({
-  selector: 'app-dettaglio',
-  templateUrl: './dettaglio.component.html',
-  styleUrls: ['./dettaglio.component.scss'],
+  selector: "app-dettaglio",
+  templateUrl: "./dettaglio.component.html",
+  styleUrls: ["./dettaglio.component.scss"],
 })
 export class DettaglioComponent {
-  nomeNazione: string = '';
+  nomeNazione: string = "";
   nazioneSingola: any;
+  mymap: any;
   constructor(
     private activatedRoute: ActivatedRoute,
     private nazioniService: NazioniService
@@ -23,9 +25,31 @@ export class DettaglioComponent {
           next: (res: any) => {
             this.nazioneSingola = res;
             console.log(this.nazioneSingola);
+            this.initializeMap();
           },
         });
       },
     });
+  }
+
+  initializeMap() {
+    this.mymap = L.map("mapid").setView(
+      [this.nazioneSingola[0].latlng[0], this.nazioneSingola[0].latlng[1]],
+      5
+    );
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.mymap);
+
+    this.mymap.on("click", this.onMapClick);
+  }
+
+  onMapClick(e: any) {
+    L.popup()
+      .setLatLng(e.latlng)
+      .setContent("You clicked the map at " + e.latlng.toString())
+      .openOn(this.mymap);
   }
 }
